82418c655233188c7e5aca68b57bff569b16ca0f
L1nJIayu <562316920@qq.com>
2024-10-25 20:44:14 +0800

    base

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/MyBlog/",
   title: "Jeffrey Lam的个人站点",
   description: "个人站点",
   themeConfig: {
